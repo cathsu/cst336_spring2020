00e7661895b53bac5f7e27dc523305682325d6b1
EC2 Default User <ec2-user@ip-172-31-36-239.us-east-2.compute.internal>
2020-02-16 04:39:55 +0000

    Initial commit

---
+++ public/labs/jquery_classwork/app.js
@@ -0,0 +1,8 @@
+// select all divs and give purple background
+$('div').css("background", "purple");
+
+// select divs with class 'highlight'
+$('div.highlight').width('200px');
+
+// select div with id 'third'
+$("div[id=third]").css("border", "solid orange");
