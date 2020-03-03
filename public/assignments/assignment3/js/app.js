1b91c07cbdb2f7007b6a562b986ddb2437ba16fa
EC2 Default User <ec2-user@ip-172-31-36-239.us-east-2.compute.internal>
2020-03-03 23:03:03 +0000

    Remove classes from output

--- public/assignments/assignment3/js/app.js
+++ public/assignments/assignment3/js/app.js
@@ -3,9 +3,6 @@ document.getElementById("nameForm").addEventListener("submit", getGender);
 function getGender() {
     var name = $("#inputName").val();
     var output = $("#output"); 
-    
-    
-    
     var URL = "https://www.behindthename.com/api/lookup.json"; 
     var API_KEY = "ca122000358"; 
     $.ajax({
@@ -15,52 +12,68 @@ function getGender() {
         data: { "key": API_KEY, 
                 "name": name},
         success: function(data) {
-            output.empty();
             var result = data[0]; 
             console.log(data);
     
-            output.append('<div id="output">');
+            output.empty();
+            removeClasses(); //remove all output classes!
             
+            // if name cannot be found!
             if (data.error_code === 50) {
                 output.append('<h4 class ="error">Error: '+ data.error + '</h4>');
-                if (output.hasClass('female')) {
-                    output.removeClass('female');
-                }
-                if (output.hasClass('male')) {
-                    output.removeClass('male');
-                }
                 
             }
             else {
-                if (result.gender === 'f') {
-                    output.append('<h5>' + result.name + ' is usually associated with the female gender </h5>');
-                    output.addClass('female');
+                if (result.gender == 'f') {
+                    output.append('<h5>' + result.name + ' is usually associated with the female gender</h5>');
+                    output.addClass('femaleBackground');
+                    // output.removeClass('maleBackground');
                 }
-                else {
+                else if (result.gender == 'm') {
                     output.append('<h5>' + result.name + ' is usually associated with the male gender</h5>');
-                    output.addClass('male');
+                    output.addClass('maleBackground');
+                    // output.removeClass('femaleBackground');
+                }
+                else { //result.gender == 'fm' or 'mf'
+                    output.append('<h5>' + result.name + ' is associated with both males and females</h5>');
+                    output.addClass('bothBackground');
                 }
                 
                 output.append('<p>' + result.name + ' is used in the following cultures: </p>');
-                // output.append('<div class="list">');
                 output.append('<ul>')
-                result.usages.forEach(function(element) {
-                    var country = element.usage_full;
-                    output.append('<li class="list">' + country + '</li>')
-                    
-                }) //data.usages
                 
+                // if name is associated with only 1 gender, just print out the cultures that use it
+                if (result.gender == 'f' || result.gender == 'm') {
+                    result.usages.forEach(function(element) {
+                        var country = element.usage_full;
+                        output.append('<li class="list">' + country + '</li>')
+                        
+                    }) //data.usages
+                }
+                
+                // if name is associated with both genders, print out culture and associated gender
+                else {
+                    console.log(data.length);
+                    for (var i = 0; i<data.length; i++) {
+                        for (var j = 0; j<data[i].usages.length; j++) {
+                            var countryGender = data[i].usages[j].usage_full + "-" + data[i].usages[j].usage_gender;
+                            output.append('<li>' + countryGender + "</li>")
+                        }
+                    }
+                }
+                    
                 // change color of bullet points
                 if (result.gender === 'f') {
                         $('li').addClass('femaleList');
                 } 
-                else {
+                else if (result.gender == 'm') {
                     $('li').addClass('maleList');
+                }
+                else {
+                    $('li').addClass('bothList');
                 }
                 
                 output.append('</ul>');
-                // output.append('</div>'); //center
-                output.append('</div>'); //output
             }
      
         }, //success 
@@ -73,5 +86,13 @@ function getGender() {
         
     });//ajax
 
+}
+
+function removeClasses (){
+    var output = $("#output"); 
+    output.removeClass("femaleBackground"); 
+    output.removeClass("maleBackground"); 
+    output.removeClass("bothBackground");
+    
 }
 
