e2a1483653c642914b2b03101661674b6fea31e5
EC2 Default User <ec2-user@ip-172-31-36-239.us-east-2.compute.internal>
2020-03-03 22:12:00 +0000

    Finished

---
+++ public/assignments/assignment3/js/app.js
@@ -0,0 +1,70 @@
+document.getElementById("nameForm").addEventListener("submit", getGender);
+
+function getGender() {
+    var name = $("#inputName").val();
+    var output = $("#output"); 
+    
+    output.empty();
+    
+    var URL = "https://www.behindthename.com/api/lookup.json"; 
+    var API_KEY = "ca122000358"; 
+    $.ajax({
+      method: "GET",
+         url: URL,
+        dataType: "json",
+        data: { "key": API_KEY, 
+                "name": name},
+        success: function(data) {
+            var result = data[0]; 
+            console.log(data);
+    
+            output.append('<div id="output">');
+            
+            if (data.error_code === 50) {
+                output.append('<h4 class ="error">Error: '+ data.error + '</h4>');
+                
+            }
+            else {
+                if (result.gender === 'f') {
+                    output.append('<h5>' + result.name + ' is usually associated with the female gender </h5>');
+                    output.addClass('female');
+                }
+                else {
+                    output.append('<h5>' + result.name + ' is usually associated with the male gender</h5>');
+                    output.addClass('male');
+                }
+                
+                output.append('<p>' + result.name + ' is used in the following cultures: </p>');
+                // output.append('<div class="list">');
+                output.append('<ul>')
+                result.usages.forEach(function(element) {
+                    var country = element.usage_full;
+                    output.append('<li class="list">' + country + '</li>')
+                    
+                }) //data.usages
+                
+                // change color of bullet points
+                if (result.gender === 'f') {
+                        $('li').addClass('femaleList');
+                } 
+                else {
+                    $('li').addClass('maleList');
+                }
+                
+                output.append('</ul>');
+                // output.append('</div>'); //center
+                output.append('</div>'); //output
+            }
+     
+        }, //success 
+        
+        error: function(err) {
+            output.append('<div id="output">');
+            output.append(err);
+            output.append('</div>');
+        } //error
+        
+    });//ajax
+
+}
+
