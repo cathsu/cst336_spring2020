06ce999121cda55733445f68241bde17c0c7a8e9
EC2 Default User <ec2-user@ip-172-31-36-239.us-east-2.compute.internal>
2020-03-11 18:37:01 +0000

    Added planet pages

--- public/labs/lab6/app.js
+++ public/labs/lab6/app.js
@@ -15,12 +15,16 @@ app.get("/", function(req, res){
     res.render("index.html"); 
 }); 
 
-// app.get("/mercury", function(req, res) {
-//     res.render("mercury.html");
-// }); 
+app.get("/mercury", function(req, res) {
+    res.render("mercury.html");
+}); 
 
 app.get("/venus", function(req, res) {
-    res.send("This will be the Venus web page!"); 
+    res.send("venus.html"); 
+});
+
+app.get("/earth", function(req, res) {
+    res.send("earth.html"); 
 });
 
 //server listener
